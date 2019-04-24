4f7a34f1bd2c197ebcaa140ec8f529c88f9201a4
Butakus <butakus@gmail.com>
2019-04-24 16:23:32 +0200

    Fix laser_to_gridmap node. Add time debug.

--- src/laser_to_gridmap.cpp
+++ src/laser_to_gridmap.cpp
@@ -5,10 +5,6 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include <tf/transform_listener.h>
 
-//#include <image_transport/image_transport.h>
-
-
-using namespace std;
 
 double map_height;
 double map_width;
@@ -18,12 +14,12 @@ bool DEBUG;
 ros::Publisher laser_gridmap_pub;
 
 // Laser frame to base_footprint tf transform
-string map_frame_id, frame_vehicle;
+std::string map_frame_id, frame_vehicle;
 tf::TransformListener* tf_listener;
 
-void laser_callback(const sensor_msgs::LaserScanConstPtr scan){
-
-    ROS_INFO("Callback");
+void laser_callback(const sensor_msgs::LaserScanConstPtr scan)
+{
+    ros::Time t0 = ros::Time::now();
     // Update the laser transform
     tf::StampedTransform laser_footprint_transform;
     try
@@ -32,72 +28,89 @@ void laser_callback(const sensor_msgs::LaserScanConstPtr scan){
     }
     catch(tf::TransformException &ex)
     {
-        ROS_ERROR("%s",ex.what());
+        ROS_ERROR("%s", ex.what());
         return;
     }
-
+    if (DEBUG) std::cout << "Transform time: " << ros::Time::now() - t0 << std::endl;
+    t0 = ros::Time::now();
     geometry_msgs::Pose origin;
-    origin.position.x = - (map_width /2);
+    origin.position.x = 0.0;
     origin.position.y = - (map_height /2);
 
-    hypergrid::GridMap gridmap(map_width, map_height, cell_size, origin);
+    hypergrid::GridMap gridmap(map_width, map_height, cell_size, origin, "icab1/base_footprint");
 
-    if (DEBUG) cout << "\n------------------------\nNew laser\n";
-    
+    if (DEBUG) std::cout << "\n------------------------\nNew laser" << std::endl;
+
     af::array ranges(scan->ranges.size(), 1, scan->ranges.data());
     af::array scan_angle(scan->ranges.size(), 1);
 
-    float angle_min = scan->angle_min;
-    float angle_inc = scan->angle_increment;
-    float range_max = scan->angle_max;
-    
-    af::seq a(0,scan->ranges.size());
-    af::array i = a ;
-    
-    scan_angle = angle_min + i * angle_inc ;
-    
-    
-    af::array obstacle_x (scan->ranges.size(), 1);
-    af::array obstacle_y (scan->ranges.size(), 1);
+    af::array ranges_seq = af::seq(0, scan->ranges.size() - 1);
+    scan_angle = scan->angle_min + ranges_seq * scan->angle_increment;
 
-    af::array ranges_nan_inf = af::isNaN(ranges) + af::isInf(ranges);    
+    // Remove NaNs and Inf values
+    af::array ranges_nan_inf = af::isNaN(ranges) || af::isInf(ranges);
+    af::replace(ranges, !ranges_nan_inf, scan->range_max);
 
-    gfor(af::seq i, scan_angle.dims(0))
+    if (DEBUG) std::cout << "Init time: " << ros::Time::now() - t0 << std::endl;
+    t0 = ros::Time::now();
+
+    af::array obstacles = af::constant(1.0, scan->ranges.size(), 3, f64);
+
+    obstacles(af::span, 0) = ranges * af::cos(scan_angle);
+    obstacles(af::span, 1) = ranges * af::sin(scan_angle);
+
+    // Get Rotation and translation from TF transform
+    tf::Vector3 trans = laser_footprint_transform.getOrigin();
+    double theta = tf::getYaw(laser_footprint_transform.getRotation());
+    double t_r_array[] = {std::cos(theta),  std::sin(theta), 0,
+                          -std::sin(theta), std::cos(theta), 0,
+                          trans.getX(),     trans.getY(),    1};
+    af::array transformation(3, 3, t_r_array);
+    // Transform the obstacles to the map frame
+    obstacles = af::matmul(transformation, obstacles.T()).T();
+
+    if (DEBUG) std::cout << "Obstacles transform time: " << ros::Time::now() - t0 << std::endl;
+    t0 = ros::Time::now();
+
+    // Set the obstacles in the map
+    // Cannot use gfor because of isCellInside check
+    // TODO: Find a way to parallelize this
+    // gfor(af::seq i, scan->ranges.size())
+    for (int i = 0; i < scan->ranges.size(); ++i)
     {
-        af::array condition = (ranges_nan_inf(i).scalar<int>()==1); // good
-        ranges(i) = (!condition).as(f32) * ranges(i)  
-                    + condition.as(f32) * range_max;
+        hypergrid::Cell obs_coords = gridmap.cellCoordsFromLocal(obstacles(i, 0).scalar<double>(),
+                                                                 obstacles(i, 1).scalar<double>());
+        if (gridmap.isCellInside(obs_coords))
+        {
+            gridmap[obs_coords] = hypergrid::GridMap::OBSTACLE;
+        }
     }
 
-    obstacle_x =  ranges * af::cos(scan_angle);
-    obstacle_y =  ranges * af::sin(scan_angle);
-    
-    //update obstacle_x and obstacle_y with tf 
-    tf::Vector3 translate = laser_footprint_transform.getOrigin();
-    tf::Quaternion rotate = laser_footprint_transform.getRotation();
-    // Get Theta
-    double theta = tf::getYaw(rotate);
-    af::array temp = obstacle_x * cos(theta) - obstacle_y * sin(theta) + translate.getX();
-    obstacle_y = obstacle_x * sin(theta) + obstacle_y * cos(theta) + translate.getY();
-    obstacle_x = temp ;
-
-    // Set the obstacle in the map
-    gfor(af::seq i, scan->ranges.size()){
-        hypergrid::Cell obs_coords = gridmap.cellCoordsFromLocal(obstacle_x(i).scalar<double>(), obstacle_y(i).scalar<double>());
-        // Check if the obstacle coords lay inside the map before modifying the cell
-        if (gridmap.isCellInside(obs_coords)) gridmap.grid(obs_coords.x, obs_coords.y) = hypergrid::GridMap::UNKNOWN;
-    }
+    if (DEBUG) std::cout << "Set obstacles time: " << ros::Time::now() - t0 << std::endl;
+    t0 = ros::Time::now();
 
     // Add the free lines to all obstacles
-    gfor(af::seq i, scan->ranges.size())
+    // Cannot use gfor because of branching inside addFreeLine method and isCellInside check
+    // TODO: Find a way to parallelize this
+    // gfor(af::seq i, scan->ranges.size())
+    for (int i = 0; i < scan->ranges.size(); ++i)
     {
-        gridmap.addFreeLine(obstacle_x(i).scalar<double>(),obstacle_y(i).scalar<double>());
+        hypergrid::Cell obs_coords = gridmap.cellCoordsFromLocal(obstacles(i, 0).scalar<double>(),
+                                                                 obstacles(i, 1).scalar<double>());
+        // TODO: Draw the free line from the sensor point, and not from the vehicle center
+        // Check if the obstacle coords lay inside the map before modifying the cell
+        if (gridmap.isCellInside(obs_coords)) gridmap.addFreeLine(obstacles(i, 0).scalar<double>(),
+                                                                  obstacles(i, 1).scalar<double>());
     }
+
+    if (DEBUG) std::cout << "Set lines time: " << ros::Time::now() - t0 << std::endl;
+    t0 = ros::Time::now();
 
     // Publish OccupancyGrid map
     laser_gridmap_pub.publish(gridmap.toMapMsg());
-    
+    if (DEBUG) std::cout << "Publish time: " << ros::Time::now() - t0 << std::endl;
 }
+
 
 int main(int argc, char **argv)
 {
@@ -105,39 +118,24 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "laser_to_gridmap");
     ros::NodeHandle public_nh, private_nh("~");
-    ROS_INFO("Starting laser to gridmap");
-    string laser_topic;
-    string output_topic;
-    private_nh.param<string>("laser_topic", laser_topic, "/icab1/scan");
-    private_nh.param<string>("output_topic", output_topic, "laser_to_gridmap");
-    private_nh.param<string>("map_frame_id", map_frame_id, "/icab1/base_footprint");
-    private_nh.param("height", map_height, 150.0);
-    private_nh.param("width", map_width, 100.0);
-    private_nh.param("cell_size", cell_size, 0.1);
+
+    std::string laser_topic;
+    std::string output_topic;
+    private_nh.param<std::string>("laser_topic", laser_topic, "/icab1/scan");
+    private_nh.param<std::string>("output_topic", output_topic, "laser_to_gridmap");
+    private_nh.param<std::string>("map_frame_id", map_frame_id, "icab1/base_footprint");
+    private_nh.param("height", map_height, 50.0);
+    private_nh.param("width", map_width, 50.0);
+    private_nh.param("cell_size", cell_size, 0.2);
     private_nh.param("DEBUG", DEBUG, true);
     public_nh.param<std::string>("vehicle", frame_vehicle, "");
-
-    // ROS_INFO("Listening to %s", laser_topic);   
 
     laser_gridmap_pub = public_nh.advertise<nav_msgs::OccupancyGrid>("hypergrid/" + output_topic, 2);
     ros::Subscriber laser_sub = public_nh.subscribe(laser_topic, 5, laser_callback);
 
     tf_listener = new tf::TransformListener;
-    ROS_INFO("Ready");
 
     ros::spin();
-   
-   
-   
-    ros::Rate r(2.0);
-   /* while(ros::ok()){
-        ROS_INFO("Loop");
-        r.sleep();
-        ros::spinOnce();
-        //if(DEBUG)
-        
-    }*/
-    
 
     return 0;
 }
