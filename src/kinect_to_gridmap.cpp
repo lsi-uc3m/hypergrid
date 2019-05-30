55706abf1650bc0943c7f5f84fc29b80f6616eec
Omar El-Sobky <omar_sobky@hotmail.com>
2019-05-30 16:56:15 +0200

    creating a skeleton for the kinect to gridmap node

---
+++ src/kinect_to_gridmap.cpp
@@ -0,0 +1,8 @@
+#include <cstring>
+#include <hypergrid/gridmap.hpp>
+#include <hypergrid/conversions/lidar_converter.hpp>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <tf/transform_listener.h>
